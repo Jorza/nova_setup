480cd3562f0e24117f1611bdd23d662ec428ab84
Jory Braun <joryb98@gmail.com>
2022-02-12 21:50:09 +1100

    Added support for GUI applications

--- nova-dev-ros2/compose-run-ros2.sh
+++ nova-dev-ros2/compose-run-ros2.sh
@@ -3,6 +3,9 @@
 # Copy SSH config from the host computer
 cp -r ~/.ssh .
 
+# Give docker permission to access the X-server for running GUI applications
+xhost +local:docker
+
 # Start conatiner using up, then open an interactive session
 sudo docker-compose up -d
 sudo docker exec -it nova-dev-ros2 bash
