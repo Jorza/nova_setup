6f2c65fabdf1cef02faeceeec4da9135dce30ac2
Jory Braun <joryb98@gmail.com>
2023-02-14 16:06:00 +1100

    Updated ROS2 install script using commands from the website

--- setup-scripts/scripts-0/2.1-ros2.sh
+++ setup-scripts/scripts-0/2.1-ros2.sh
@@ -5,18 +5,16 @@ source ./scripts-0/0-script-setup.sh
 
 # Install ROS2
 information "Installing ROS 2..."
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y curl gnupg2 lsb-release
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
-sudo apt update -y
-sudo apt install -y ros-foxy-desktop
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y software-properties-common
+sudo add-apt-repository universe
+sudo apt install -y curl
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y ros-foxy-desktop python3-argcomplete
+sudo apt install -y ros-dev-tools
 source /opt/ros/foxy/setup.bash
-pip3 install -U argcomplete
-sudo apt install -y python-rosdep
-sudo rosdep init
-rosdep update -y --include-eol-distros --rosdistro=foxy
-sudo apt install -y python3-colcon-common-extensions
 
 # Install ros2 bag
 sudo apt install -y ros-foxy-ros2bag ros-foxy-rosbag2-converter-default-plugins ros-foxy-rosbag2-storage-default-plugins
